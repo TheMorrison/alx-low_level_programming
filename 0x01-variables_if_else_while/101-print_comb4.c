6048edb8e3b5176dc8a37308207b2d72567241d5
Morrison B. Boamah <morrisonl.boamah@gmail.com>
2023-02-17 23:33:21 -0800

    3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,11 +18,11 @@ int main(void)
 		while (y <= 8)
 		{
 			z = y + 1;
-			while (k <= 9)
+			while (z <= 9)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-				putchar(k + '0');
+				putchar(z + '0');
 				if (count != 119)
 				{
 					putchar(',')
