9902e02608054a43d3ad0bd404c10e828d741d1a
Morrison B. Boamah <morrisonl.boamah@gmail.com>
2023-02-17 23:22:49 -0800

    3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible different combinations of three digits
+ * @void: Null value
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+	int y, k;
+	int count = 0;
+
+	while (x <= 7)
+	{
+		y = x + 1;
+		while (y <= 8)
+		{
+			z = y + 1;
+			while (k <= 9)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (count != 119)
+				{
+					putchar(',')
+						putchar(' ')
+				}
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
